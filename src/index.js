814a61f6bc73ebf7e7a5247a66de0421bb938ecc
andoski99 <goodsportdevelopment@icloud.com>
2022-05-13 05:51:01 +1000

    Set up for the toDo lists

--- src/index.js
+++ src/index.js
@@ -8,20 +8,23 @@ const extraRecord = {
 };
 
 const before = document.getElementById("before");
-before.innerHTML = JSON.stringify(companiesData, null, 2);
 
-//TODO: Extract an element from the array.
-//TODO: Add elements to beginning and end of array.
-//TODO: Remove Elements from end of array.
-//TODO: Create a new array from old data.
-//TODO: Filter for a certain state.
-//TODO: Find the first element with a certain name.
-//TODO: Create a new array with just elements of a certain state.
-//TODO: Create buttons for each record.
+
+//TODO: Get and array of the unique states.
+//TODO: Get each state in its own card/div (Parent element for each state)
+//TODO: Show the state in each parent element title
+//TODO: get data (company) for each state
+//TODO: create button for each company
+//TODO: append button to each parent element
+//TODO: append the parent element to the after div
+//TODO:
+
+
 //functions are here
+const filterFunction = (company) =>
+  company.fieldData.State === "TX";
 
-const forEachFunction = (company) => {
-// delete company.City;
+const createButton = (company) => {
 const btn = document.createElement("button");
 btn.type = "button";
 btn.innerHTML = company.Company;
@@ -29,26 +32,8 @@ btn.className = "btn btn-primary";
 after.appendChild(btn);
 };
 
-const filterFunction = (company) =>
-  company.fieldData.State === "TX" ||
-  company.fieldData.State === "CA" ||
-  company.fieldData.State === "NY" ||
-  company.fieldData.State === "WA" ||
-  company.fieldData.State === "IL" ||
-  company.fieldData.State === "SD";
  
-const mapFunction = (company) => {
-  const newObj = {
-    State: company.fieldData.State,
-    City: company.fieldData.City,
-    StreetAddress: company.fieldData.StreetAddress,
-    Company: company.fieldData.CompanyName,
-    ID: company.fieldData.Id,
-    ZIP: company.fieldData.Zip,
-  };
-  return newObj;
-};
 
-const manipulatedData = companiesData.filter(filterFunction).map(mapFunction)
-manipulatedData.forEach(forEachFunction);
-// after.innerHTML = JSON.stringify(manipulatedData, null, 2);
+
+
+
