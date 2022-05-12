5c362d7763d57bf822cf349a02e28db88e773b38
andoski99 <goodsportdevelopment@icloud.com>
2022-05-13 07:01:03 +1000

    Finished Lesson

--- src/index.js
+++ src/index.js
@@ -1,38 +1,58 @@
 import { companiesData } from "./companies-data";
 
-const extraRecord = {
-  title: "JS in FM Training",
-  type: "Binge Mode",
-  cohort: "2",
-  startDate: "2022-04-12",
-};
-
-const before = document.getElementById("before");
-
-
 //TODO: Get and array of the unique states.
-//TODO: Get each state in its own card/div (Parent element for each state)
+const states = companiesData.map((company) => company.fieldData.State);
+// console.log(states);
+
+const findUnique = (value, index, self) => {
+  return self.indexOf(value) === index;
+};
+const uniqueStates = states.filter(findUnique).sort();
+// console.log(uniqueStates); 
+
+//TODO: Get each state in its own card/div (Parent element for each state
+const createParent = (state) => {
+  const parent = document.createElement("div");
+  parent.className = "col-4 p-1 ";
+  parent.innerHTML = `<h2> ${state} </h2>`;
+return parent;
+}
 //TODO: Show the state in each parent element title
 //TODO: get data (company) for each state
 //TODO: create button for each company
 //TODO: append button to each parent element
 //TODO: append the parent element to the after div
-//TODO:
 
-
-//functions are here
-const filterFunction = (company) =>
-  company.fieldData.State === "TX";
+// const filterFunction = (company,state) => company.fieldData.State === state;
+const filterForState = (array, state) => {
+  console.log(array);
+  console.log(state);
+  return array.filter((company) => company.fieldData.State === state);
+}
+const checkThis = filterForState(companiesData, "TX");
+console.log(checkThis);
 
 const createButton = (company) => {
 const btn = document.createElement("button");
 btn.type = "button";
-btn.innerHTML = company.Company;
-btn.className = "btn btn-primary";
-after.appendChild(btn);
+btn.innerHTML = company;
+btn.className = "col-12 m-1 btn btn-primary";
+return btn;
+// after.appendChild(btn);
 };
 
- 
+//Ultimate Function
+uniqueStates.forEach((state) => {
+  const stateData = filterForState(companiesData, state);
+  const parentElement = createParent(state);
+  stateData.forEach((company) => {
+    const CompanyName = company.fieldData.CompanyName;
+    const btn = createButton(CompanyName);
+    parentElement.appendChild(btn);
+  });
+  console.log(stateData);
+  after.appendChild(parentElement);
+});
 
 
 
